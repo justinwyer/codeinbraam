05caa9bc04687b6d244e386d92fcaed8b2f68f64
Justin Wyer <justin@lifeasageek.com>
2012-07-15 12:44:33 +0200

    Initail import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twit::Application.config.secret_token = '3921b426823477b89add94d452de98107d6acf4bb6993c59e8615218173cfc42179b96a8b1c8f825b2dc2ca922d5a0a5398901ad223601360ca4c9f2a45fcc90'
